318485bae5c3a1de438f0c3c1947783769c8a9ca
Việt Bùi Hà <buihaviet@gmail.com>
2021-03-13 16:22:57 +0700

    13/3/2021

--- command.cs
+++ command.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using System.Linq;
 using System.Windows;
 
 namespace WpfControlLibrary1
@@ -11,14 +12,33 @@ namespace WpfControlLibrary1
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            var eles = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralColumns)
+                .WhereElementIsNotElementType()
+                .ToElements();
 
+            using (var tran = new Transaction(doc, "Join Floor and Column"))
+            {
+                tran.Start();
+                foreach (var ele in eles)
+                {
+                    var boundingBox = ele.get_BoundingBox(null);
+                    var outline = new Outline(boundingBox.Min, boundingBox.Max);
 
+                    var filter = new BoundingBoxIntersectsFilter(outline);
+                    var floors = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors)
+                        .WhereElementIsNotElementType()
+                        .WherePasses(filter)
+                        .ToElements();
 
-            MessageBox.Show("Hello work!");
-
-
-
-
+                    foreach (var item in floors)
+                    {
+                        JoinGeometryUtils.JoinGeometry(doc, ele, item);
+                        JoinGeometryUtils.SwitchJoinOrder(doc, ele, item);
+                    }
+                }
+                tran.Commit();
+            }
             return Result.Succeeded;
         }
     }
