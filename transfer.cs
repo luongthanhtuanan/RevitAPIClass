c39b5644a10ad37be36fb3726da6240746ea9cc2
quyetmt <80567242+quyetmt@users.noreply.github.com>
2021-03-24 13:55:46 +0700

    commit

--- transfer.cs
+++ transfer.cs
@@ -4,9 +4,12 @@ using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System.Linq;
 using System.Windows;
+using System.Collections.Generic;
 
 namespace WpfControlLibrary1
 {
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
     class transfer : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
@@ -17,16 +20,31 @@ namespace WpfControlLibrary1
 
             //Get elements of Category
             var eles = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralColumns)
-                .WhereElementIsNotElementType()                
-                .ToElementIds();
+                .WhereElementIsNotElementType()              
+                .ToElements();
+            var floors = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors)
+                .WhereElementIsNotElementType()
+                .ToElements();
 
-            foreach (var item in collection)
+            using (var tran = new Transaction(doc, "Join Columns and Floors"))
             {
-
-            }
-
-
-            return Result.Succeeded;
+                tran.Start();
+                foreach (var ele in eles)
+                {
+                    if (ele.Name == "300 x 450mm")
+                    {
+                        {
+                            foreach (var item in floors)
+                            {
+                                
+                                JoinGeometryUtils.JoinGeometry(doc, ele, item);
+                            }
+                        }
+                    }
+                }
+                tran.Commit();
+            }                  
+            return Result.Succeeded;         
         }
     }    
 }
